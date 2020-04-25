aea54c6b7c8e88d89e963bde446623d782b5e3a6
Karan Gore <49974198+itskgore@users.noreply.github.com>
2020-04-25 17:05:06 +0530

    Update listData.dart

--- lib/widgets/listData.dart
+++ lib/widgets/listData.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:nitorinfotech/widgets/CustomDialog.dart';
-import 'package:nitorinfotech/widgets/Urlwidget.dart';
+import '../widgets/CustomDialog.dart';
+import '../widgets/Urlwidget.dart';
 
 class ListTileData extends StatelessWidget {
   final String username;
